273c812c6fdcdc57f58ece58cc69fcafc1d43e83
itsfarhankhan28 <famannat@gmail.com>
2023-08-14 17:59:58 +0530

    feat(db): configured dotenv

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 const POOL = require('pg').Pool
 
 const pool = new POOL({
-    user:"farhanapi",
-    password:"farhankhan",
-    host:"localhost",
-    port:5432,
-    database:"recipeapi"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
 })
 
 module.exports = pool
