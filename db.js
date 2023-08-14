351f8a2c8472012cecbae278d1c1a5396d46d4a9
itsfarhankhan28 <famannat@gmail.com>
2023-08-15 01:08:56 +0530

    fix(db): changed the configuration of the database

--- db.js
+++ db.js
@@ -3,11 +3,12 @@ require('dotenv').config();
 const POOL = require('pg').Pool
 
 const pool = new POOL({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    // database: process.env.DB_NAME
+    connectionString:process.env.INTERNAL_URL
 })
 
 module.exports = pool
