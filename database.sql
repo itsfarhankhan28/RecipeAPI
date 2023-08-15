1051dab0a97091c29cd188d318e3bce98fe9fff3
itsfarhankhan28 <famannat@gmail.com>
2023-08-15 13:36:59 +0530

    feat(database): changed the varchar value

--- database.sql
+++ database.sql
@@ -3,9 +3,9 @@ CREATE DATABASE recipeapi;
 CREATE TABLE recipes(
     recipe_id SERIAL PRIMARY KEY,
     title CHAR(20),
-    description VARCHAR(255),
-    ingredients_name VARCHAR(255),
-    instructions VARCHAR(255),
+    description VARCHAR(500),
+    ingredients_name VARCHAR(500),
+    instructions VARCHAR(500),
     prep_time INT,
     cook_time INT,
     total_time INT ,
